67dfaae205b7bbd7b7c20a10e79a12d8b9577730
Wayne Sun <wtse.sun@gmail.com>
2019-09-11 15:40:35 -0700

    created questionlist and sectionbar components

---
+++ src/SectionBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SectionBar() {
+  return (
+    <div className="section-bar-box">
+      <button>Teamwork</button>
+      <button>Client-facing Skills</button>
+      <button>Ability to Adapt</button>
+      <button>Time Management Skills</button>
+      <button>Communication Skills</button>
+    </div>
+  )
+}
